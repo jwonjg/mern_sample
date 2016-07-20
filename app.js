6d21fe64f965c2fceb8986fc7bf9170b5838e3d0
jjw <jwonjg@naver.com>
2016-07-20 22:47:29 +0900

    Listing cities on index.html

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public')); // default index property is 'index.html'
+
 app.get('/', function(request, response) {
   response.send('OK');
+});
+
+app.get('/cities', function(request, response) {
+  response.json(['Lotopia', 'Caspiana', 'Indigo']);
 });
 
 module.exports = app;
--- bin/www.js
+++ bin/www.js
@@ -1,4 +1,4 @@
 var app = require('./../app');
 app.listen(3000, function() {
   console.log('Listening on port 3000');
-})
+});
