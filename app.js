887af2df56283aa0efbd07caf3ac9221f0e20b44
jjw <jwonjg@naver.com>
2016-07-22 00:35:33 +0900

    Replace in memory with redis code added

--- app.js
+++ app.js
@@ -6,22 +6,49 @@ var urlencoded = bodyParser.urlencoded({ extended: false});
 
 app.use(express.static('public')); // default index property is 'index.html'
 
+// * redis
+// var redis = require('redis');
+// if (process.env.REDISTOGO_URL) {
+//   var rtg = require("url").parse(process.env.REDISTOGO_URL);
+//   var client = redis.createClient(rtg.port, rtg.hostname);
+//   client.auth(rtg.auth.split(":")[1]);
+// } else {
+//   var client = redis.createClient();
+// }
+// client.select((process.env.NODE_ENV || 'development').length);
+
+// * redis
+// client.hset('cities', 'Lotopia', 'some description');
+// client.hset('cities', 'Caspiana', 'some description');
+// client.hset('cities', 'Indigo', 'some description');
+
 var cities = {
   'Lotopia':'some description',
   'Caspiana':'some description',
   'Indigo':'some description'
 };
 
-app.get('/', function(request, response) {
-  response.send('OK');
-});
+// unreachable. cause default index property takes request
+// app.get('/', function(request, response) {
+//   response.send('OK');
+// });
 
 app.get('/cities', function(request, response) {
+  // * redis
+  // client.hkeys('cities', function(error, names) {
+  //   if(error) throw error;
+  //   response.json(names);
+  // });
   response.json(Object.keys(cities));
 });
 
 app.post('/cities', urlencoded, function(request, response) {
   var newCity = request.body;
+  // * redis
+  // client.hset('cities', newCity.name, newCity.description, function(error) {
+  //   if(error) throw error;
+  //   response.status(201).json(newCity.name);
+  // });
   cities[newCity.name] = newCity.description;
   response.status(201).json(newCity.name);
 });
