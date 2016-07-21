07d67c3b25f496e1ad4fe7a150a9de73a5132776
jjw <jwonjg@naver.com>
2016-07-21 23:08:21 +0900

    Create new cities

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var urlencoded = bodyParser.urlencoded({ extended: false});
+
 app.use(express.static('public')); // default index property is 'index.html'
+
+var cities = {
+  'Lotopia':'some description',
+  'Caspiana':'some description',
+  'Indigo':'some description'
+};
 
 app.get('/', function(request, response) {
   response.send('OK');
 });
 
 app.get('/cities', function(request, response) {
-  response.json(['Lotopia', 'Caspiana', 'Indigo']);
+  response.json(Object.keys(cities));
+});
+
+app.post('/cities', urlencoded, function(request, response) {
+  var newCity = request.body;
+  cities[newCity.name] = newCity.description;
+  response.status(201).json(newCity.name);
 });
 
 module.exports = app;
